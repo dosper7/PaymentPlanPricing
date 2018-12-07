c0117f18b4a413fbf23e9bb776b2b9afd217c6ca
David Pereira <david.pereira@ocidental.pt>
2018-12-07 11:59:13 +0000

    added simple testing

--- src/tests/slider.test.js
+++ src/tests/slider.test.js
@@ -6,9 +6,21 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({adapter: new Adapter()});
 
 describe(' <Slider/> testing', () =>{
+
+
     it('should render a slider',() =>{
         const slider = shallow(<Slider SelectedLoanAmount={1000} Min={100} Max={2000} Step={100}/>);
+        expect(slider).toMatchSnapshot();
+    });
+
+    it('should contain a input type range',() =>{
+        const slider = shallow(<Slider/>);
         expect(slider.find('input[type="range"]').length).toEqual(1);
+    });
+
+    it('input range should have the same value as passed in the prop "SelectedLoanAmount=1000" ',() =>{
+        const slider = shallow(<Slider SelectedLoanAmount={1000}/>);
+        slider.find('input[type="range"]').get(0).value=="1000";
     });
 });
 
